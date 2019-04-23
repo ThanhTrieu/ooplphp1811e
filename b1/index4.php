5a99c2ac1423d010fa1d64accd94d5fb94d2d49c
trieunt90 <nguyenthanhtrieu90@gmail.com>
2019-04-23 23:01:49 +0700

    first code

---
+++ b1/index4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Demo oop</title>
+</head>
+<body>
+	<h1>Checking number</h1>
+	<form action="server/checking.php" method="POST">
+		<label for="number"> Number</label>
+		<input type="number" name="number" id="number">
+		<br><br>
+		<button type="submit" name="btnCheck"> Check</button>
+	</form>
+</body>
+</html>
